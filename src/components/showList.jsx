f3e1b6d67a530534431a38ab83aaca6e9f9ba591
MrSinaf <mrsinaf@sinafproduction.com>
2023-12-15 11:49:59 +0100

    Update filter terminology and implement useState
    The filter label has been updated from "Total" to "All" for improved clarity. In addition, React's useState hook has been implemented in 'App.jsx' to manage the 'spents' and 'filtre' states, providing a more optimized way to manage state changes in our functional component. A semicolon has also been added at the end of the export statement.

--- src/components/showList.jsx
+++ src/components/showList.jsx
@@ -2,7 +2,7 @@ function ShowList({spents, categoryFiltre}) {
     return (
         <>
             {spents.map((item, index) => {
-                if (categoryFiltre === "Total" || item.category === categoryFiltre)
+                if (categoryFiltre === "All" || item.category === categoryFiltre)
                     return <div key={index} className={'spent'}>
                         <p>{item.spent}</p>
                         <p>{item.category}</p>
