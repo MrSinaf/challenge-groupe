be25a90b1fcc0a7db19f70a71f0ffb4255411652
MrSinaf <mrsinaf@sinafproduction.com>
2023-12-15 11:20:05 +0100

    Ajout de l'affichage de la liste

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import './App.css'
+import './App.css';
+import ShowList from "./compents/showList.jsx";
 
 function App() {
 
     return (
         <>
-
+            <ShowList spents={[
+                {spent: 15, category: "miam"},
+                {spent: 30, category: "tic"},
+                {spent: 40, category: "tic"},
+                {spent: 50, category: "tic"},
+                {spent: 415, category: "tac"}]} categoryFiltre={'tic'}/>
         </>
     )
 }
